adc4e80dc982011b36958092c6c17b38a8046f37
Morrivs <rmontero@camsoft.com.do>
2026-04-17 12:59:44 -0400

    prod

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { useAuthStore } from '../store/auth.store';
 
 export const api = axios.create({
-  //URL base para todas las peticiones, se lee de las variables de entorno
-  baseURL: import.meta.env.VITE_API_URL_DEV || import.meta.env.VITE_API_URL,
+  //URL
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true
 });
 
